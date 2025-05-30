08e3a59c47eb031b24855fe1e2eac5c68816e391
Lunce5in3 <2655768716@qq.com>
2025-05-30 12:09:33 +0800

    feature：2.字母异位词

--- src/main/java/Test/Solution.java
+++ src/main/java/Test/Solution.java
@@ -1,7 +1,6 @@
 package Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
     /**
@@ -16,8 +15,35 @@ public class Solution {
             if(map.containsKey(target-nums[i])){
                 return new int[]{map.get(target-nums[i]),i};
             }
-
             map.put(nums[i],i);
         }
+    }
+
+    /**
+     * 2.字母异位词
+     * @param strs
+     * @return
+     */
+    //为什么使用 String.valueOf(char[]) 而不是 toString() 方法因为返回的引用，而String.valueOf(char[]) 返回的是 字符串。
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //创建了一个map
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            //遍历字符数组中的所有，字符串转化为字符数组。
+            char[] charArray = str.toCharArray();
+            //字符串转化的数组排序
+            //排序之后，只要是 字母异位词 那么得到的字符数组就是一样的
+            Arrays.sort(charArray);
+            //如果包含了 这个字母异位词 那么就加入 这个词的map 对应的list 中
+            if(map.containsKey(String.valueOf(charArray))){
+                map.get(String.valueOf(charArray)).add(str);
+            }else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(String.valueOf(charArray), list);
+            }
+
+        }
+        return new ArrayList<>(map.values());
     }
 }
