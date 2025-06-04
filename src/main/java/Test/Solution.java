d7dc64284c2060d9d6be0d78a2983fa9b62974cd
Lunce5in3 <2655768716@qq.com>
2025-06-04 17:11:25 +0800

    feature：5.三数之和

--- src/main/java/Test/Solution.java
+++ src/main/java/Test/Solution.java
@@ -93,4 +93,37 @@ public class Solution {
             }
         }
     }
+
+    /**
+     * 5.三数之和
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int x = nums[i];
+            if( i > 0 && x == nums[i - 1] ){
+                continue;
+            }
+            int j = i + 1 , k = nums.length - 1;
+
+            while (j < k) {
+                int s = nums[i] + nums[j] + nums[k];
+                if(s > 0){
+                    k-- ;
+                }else if(s < 0){
+                    j++ ;
+                }else {
+                    res.add(List.of(x,nums[j],nums[k]));
+                    for (j++; j < k && nums[j] == nums[j - 1]; j++); // 跳过重复数字
+                    for (k--; k > j && nums[k] == nums[k + 1]; k--); // 跳过重复数字
+                }
+
+            }
+        }
+        return res;
+    }
 }
