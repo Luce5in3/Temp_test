754f396d5c79f7ede9c993888797914bb3aa7a55
Lunce5in3 <2655768716@qq.com>
2025-06-06 13:42:13 +0800

    update：10.和为k的子数组个数 移动到solution 下

--- src/main/java/Test/Solution1.java
+++ src/main/java/Test/Solution1.java
@@ -6,28 +6,7 @@ import java.util.Map;
 public class Solution1 {
 
 
-    public int subarraySum(int[] nums, int k) {
-        //构造一个前缀和数组
-        int n = nums.length;
-        int s[] = new int[n + 1];
-        //HashMap 用作统计出现过和为k 的子数组个数
-        for (int i = 0; i < n; i++) {
-            s[i + 1] = s[i] + nums[i];
 
-        }
-        int ans = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int sj : s) {
-            //我们遍历每个 sj = s[j+1]，在哈希表 cnt 中统计出现过的前缀和 s[i] 的数量。
-            ans += map.getOrDefault(sj - k, 0);
-            map.merge(sj, 1, Integer::sum);
-        }
-        return ans;
-        /*
-        s[j+1] - s[i] == k
-        => s[i] == s[j+1] - k
-        */
-    }
 
 
 }
