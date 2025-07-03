b73287781a6ae52f183bdf2bc7a68e6c6f02907a
Lunce5in3 <2655768716@qq.com>
2025-07-03 18:58:15 +0800

    feat ： nothing

--- src/main/java/Test/Main.java
+++ src/main/java/Test/Main.java
@@ -1,11 +1,16 @@
 package Test;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("熏弟这次不一样了，这是我新建的分支不会修改你的main");
 
-        //第一次提交
+    while(true){
+        Scanner sc  = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.toLowerCase());
+    }
     }
 }
 
