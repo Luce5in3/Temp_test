359cb4e462c9bfd95a1e703a3ec1a468a269e500
Luce5in3 <2655768716@qq.com>
2025-11-26 19:52:14 +0800

    feat : 测试

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+
+    //你好
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/Test/Solution.java
@@ -0,0 +1,10 @@
+package Test;
+
+import java.awt.font.TextHitInfo;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        ThreadGroup group = new ThreadGroup("Test");
+    }
+}
